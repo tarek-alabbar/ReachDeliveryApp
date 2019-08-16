ae97eb7c02b8a3469f76ac911bfa9adac6614b17
Tarek Alabbar <tarek.alabbar@fdmgroup.com>
2019-08-16 10:11:24 +0100

    Webpage working, needs refactor

--- src/main/java/com/fdmgroup/reachdeliveryapp/Controllers/MainController.java
+++ src/main/java/com/fdmgroup/reachdeliveryapp/Controllers/MainController.java
@@ -1,9 +1,17 @@
 package com.fdmgroup.reachdeliveryapp.Controllers;
 
+import com.fdmgroup.reachdeliveryapp.Model.Entities.Address;
+import com.fdmgroup.reachdeliveryapp.Model.Entities.City;
 import com.fdmgroup.reachdeliveryapp.Model.Entities.Parcel;
 import com.fdmgroup.reachdeliveryapp.Model.Entities.Reach_users;
+import com.fdmgroup.reachdeliveryapp.Model.Entities.Status;
+import com.fdmgroup.reachdeliveryapp.Model.Entities.UserDetails;
 import com.fdmgroup.reachdeliveryapp.Model.Entities.UserType;
+import com.fdmgroup.reachdeliveryapp.Model.Services.AddressImp;
+import com.fdmgroup.reachdeliveryapp.Model.Services.CityImp;
 import com.fdmgroup.reachdeliveryapp.Model.Services.ParcelImp;
+import com.fdmgroup.reachdeliveryapp.Model.Services.StatusImp;
+import com.fdmgroup.reachdeliveryapp.Model.Services.UserDetailsImp;
 import com.fdmgroup.reachdeliveryapp.Model.Services.UserTypeImp;
 import com.fdmgroup.reachdeliveryapp.Model.Services.UsersImp;
 
@@ -20,13 +28,33 @@ public class MainController {
     @RequestMapping(value = "/")
     public String indexPage(Model model){
         UsersImp usersImp = new UsersImp();
+        ParcelImp parcelImp = new ParcelImp();
+        UserTypeImp userTypeImp = new UserTypeImp();
+        AddressImp addressImp = new AddressImp();
+        CityImp cityImp = new CityImp();
+        StatusImp statusImp = new StatusImp();
+        UserDetailsImp userDetailsImp = new UserDetailsImp();
+        
+        
         Reach_users user = usersImp.Read(1);
-        // UserTypeImp userTypeImp = new UserTypeImp();
-        // UserType userType = userTypeImp.Read(user.getUser_type().getType_id());
-        // ParcelImp parcelImp = new ParcelImp();
-        // Parcel parcel = parcelImp.Read(1);
+        UserType userType = userTypeImp.Read(user.getUser_type().getType_id());
+        Address address = addressImp.Read(user.getAddress().getAddress_id());
+        UserDetails userDetails = userDetailsImp.Read(user.getUsers_details().getDetails_id());
+        
+        Parcel parcel = parcelImp.Read(1);
+        Status status = statusImp.Read(parcel.getStatus().getStatus_id());
+        City fromCity = cityImp.Read(parcel.getFrom().getCity_id());
+        City destinationCity = cityImp.Read(parcel.getDestination().getCity_id());
+         
         model.addAttribute("userObj", user);
-        // model.addAttribute("userType", userType);
+         model.addAttribute("userTypeObj", userType);
+         model.addAttribute("addressObj", address);
+         model.addAttribute("userDetailsObj", userDetails);
+         
+         model.addAttribute("parcelObj", parcel);
+         model.addAttribute("statusObj", status);
+         model.addAttribute("fromCityObj", fromCity);
+         model.addAttribute("destinationCityObj", destinationCity);
         return "index";
     }
 
--- src/main/webapp/WEB-INF/views/index.jsp
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -10,7 +10,25 @@
 
 	<h1>Welcome ${userObj.name}</h1>
 	<h2>Your username is ${userObj.username}</h2>
-	<%-- <h2>Your user type is: ${userType.description}</h2> --%>
+	
+	<h2>Your user type is: ${userTypeObj.description}</h2>
+	<h2>You Address is</h2>
+	<h3>City ${addressObj.city}</h3>
+	<h3>House number ${addressObj.house_number}</h3>
+	<h3>Street ${addressObj.street}</h3>
+	<h3>Postcode ${addressObj.postcode}</h3>
+	
+	<h2>Your user details are</h2>
+	<h3>Your email is ${userDetailsObj.email}</h3>
+	<h3>Your Phone Number is ${userDetailsObj.phoneNumber}</h3>
+	
+	<h2>Your parcel info</h2>
+	<h3>Tracking number ${parcelObj.tracking_number}</h3>
+	<h3>Parcel status ${statusObj.description}</h3>
+	<h3>Coming from ${fromCityObj.city_name}</h3>
+	<h3>Going to ${destinationCityObj.city_name}</h3>
+	
+	
 	
 </body>
 </html>
